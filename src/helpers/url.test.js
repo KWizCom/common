be3037f78acab8f2df3331c20f5a1a101f85da4e
Shai Petel <shai@petel.com>
2024-03-04 20:27:38 -0500

    fix makeFullUrl with baseUrl

--- src/helpers/url.test.js
+++ src/helpers/url.test.js
@@ -1,6 +1,6 @@
 import assert from 'assert/strict';
 import test from 'node:test';
-import { parseHash, parseQueryString } from './url';
+import { makeFullUrl, parseHash, parseQueryString, } from './url';
 
 test('parseQueryString', t => {
     assert.deepEqual(parseQueryString(), {});
@@ -15,4 +15,11 @@ test('parseHash', t => {
     assert.deepEqual(parseHash("http://foo?p1=1&p2=2#someAnchor"), { someAnchor: '' });
     assert.deepEqual(parseHash("http://foo?p1=3&p2=4#p1=1&p2=2"), { p1: '1', p2: '2' });
     assert.deepEqual(parseHash("http://foo#p1=1&p2=2"), { p1: '1', p2: '2' });
+});
+
+test('makeFullUrl', t => {
+    assert.deepEqual(makeFullUrl("http://foo"), "http://foo");
+    assert.deepEqual(makeFullUrl("http://foo", "http://fii"), "http://foo");
+    assert.deepEqual(makeFullUrl("foo/fii", "http://host/folder"), "http://host/folder/foo/fii");
+    assert.deepEqual(makeFullUrl("/foo/fii", "http://host"), "http://host/foo/fii");
 });
