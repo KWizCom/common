6b66b177b6994454ad16e3fa7979a9ba2b6a9988
Kevin Vieira <kevin@kwizcom.com>
2024-03-12 14:23:34 -0400

    updated missing exported script and sod, updated UserHasManageSitePermissions

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+import { default as _script } from "./utils/script";
+import { default as _sod } from "./utils/sod";
 export * from './exports-index';
+export const script = _script;
+export const Sod = _sod;
 
