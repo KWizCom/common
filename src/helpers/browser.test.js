c0a2ba40ea6505e4357d6639a54a7357d43e44f2
Shai Petel <shai@petel.com>
2024-08-02 14:45:56 -0400

    noMetadata for JSON request get file last modified, more unit test

--- src/helpers/browser.test.js
+++ src/helpers/browser.test.js
@@ -1,9 +1,13 @@
 import assert from 'assert/strict';
 import test from 'node:test';
-import { DisableAnchorInterceptInHtml } from "./browser";
+import { DisableAnchorInterceptInHtml, HTMLDecode } from "./browser";
 
 test('DisableAnchorInterceptInHtml', async t => {
     assert.strictEqual(DisableAnchorInterceptInHtml(`<a href="blah">test</a>`), `<a data-interception="off" href="blah">test</a>`);
     assert.strictEqual(DisableAnchorInterceptInHtml(`<div><a href="blah">test</a><a href="blah">test</a></div>`), `<div><a data-interception="off" href="blah">test</a><a data-interception="off" href="blah">test</a></div>`);
     assert.strictEqual(DisableAnchorInterceptInHtml(`<p href="blah">test</p>`), `<p href="blah">test</p>`);
+});
+
+test('HTMLDecode', async t => {
+    assert.strictEqual(HTMLDecode(`hello &lt; &#58;`), `hello < :`);
 });
